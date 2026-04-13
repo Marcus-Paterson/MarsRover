bf280605ac58a7adfc459fdfec51b05bfbac5b32
Marcus Paterson <marcus_paterson@outlook.com>
2026-04-13 16:44:05 +0100

    FEAT: Added functioanlity to ask for user input on Program.cs

--- MarsRover.Console/Program.cs
+++ MarsRover.Console/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace MarsRover_Console
+﻿using MarsRover_Console.ParseInput;
+using MarsRover_Console.Parsers;
+
+namespace MarsRover_Console
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+
+            Console.WriteLine("Enter the plateau size (This should be 5 5):");
+            string userPlateauSizeInput = Console.ReadLine();
+            InputPlateauSizeParse.ParsePlateauSize(userPlateauSizeInput);
+
+
+            Console.WriteLine("Enter the rover's initial position and direction. Do this by entering two numbers under or equal to 5 and a direction(N,E,S or W) seperate everything with a space, e.g. 1 2 N):");
+            string userRoverPositionInput = Console.ReadLine();
+            InputRoverParse.ParseStartingPosition(userRoverPositionInput);
+
+            Console.WriteLine("Enter the instructions for the rover. You must use L, R or M. Enter your instructions with no space, e.g. LMRMMRML):");
+            string userInstructionInput = Console.ReadLine();
+            InputInstructionParse.ParseInstructions(userInstructionInput);
+
         }
     }
 }
